e07244cacef7bfd5d3ada0cefce50df1042ff05f
yuanguozheng <yuanguozheng@outlook.com>
2020-08-06 23:36:35 +0800

    Bitmap扩展添加图片旋转

--- kotlinextensions/src/main/kotlin/com/coderyuan/kotlinextensions/image/BitmapExtensions.kt
+++ kotlinextensions/src/main/kotlin/com/coderyuan/kotlinextensions/image/BitmapExtensions.kt
@@ -19,12 +19,41 @@ fun Bitmap.scale(newMaxSize: Int, autoRecycleOrigin: Boolean = true): Bitmap? {
     }
     val matrix = Matrix()
     matrix.postScale(rate, rate)
+    val newBM: Bitmap? = createNewBitmapByMatrix(this, matrix)
+    if (!this.isRecycled && autoRecycleOrigin) {
+        this.recycle()
+    }
+    return newBM
+}
+
+/**
+ * 旋转Bitmap
+ *
+ * @param degree 角度
+ * @param autoRecycleOrigin 是否自动回收原图
+ */
+fun Bitmap.rotate(degree: Int, autoRecycleOrigin: Boolean = true): Bitmap? {
+    val matrix = Matrix()
+    matrix.postRotate(degree.toFloat())
+    val newBM: Bitmap? = createNewBitmapByMatrix(this, matrix)
+    if (!this.isRecycled && autoRecycleOrigin) {
+        this.recycle()
+    }
+    return newBM
+}
+
+/**
+ * 使用Matrix创建一个新的Bitmap
+ *
+ * @param bitmap 需要处理的Bitmap
+ * @param matrix 目标Matrix
+ */
+private fun createNewBitmapByMatrix(bitmap: Bitmap, matrix: Matrix): Bitmap? {
     var newBM: Bitmap? = null
+    val height = bitmap.height
+    val width = bitmap.width
     try {
-        newBM = Bitmap.createBitmap(this, 0, 0, width, height, matrix, false)
-        if (!this.isRecycled && autoRecycleOrigin) {
-            this.recycle()
-        }
+        newBM = Bitmap.createBitmap(bitmap, 0, 0, width, height, matrix, false)
     } catch (e: Exception) {
         e.printStackTrace()
     }
